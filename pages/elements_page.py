d4d5bc167539090dd4ca0cef82efbf12d3d13f93
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-07 20:32:34 +0300

    Update elements_page.py

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -7,3 +7,7 @@ class ElementPage(BasePage):
         self.base_url = 'https://demoqa.com/elements'
         super().__init__(driver, self.base_url)
 
+
+        self.get_center_text = WebElement(self.driver, ".col-12.mt-4.col-md-6")
+       
+
