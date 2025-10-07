2d3ac5defec0a4fc6479abc8582063b2c85d62f9
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-07 20:34:17 +0300

    Update test_check_text.py

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -10,5 +10,18 @@ def test_text_footer(browser):
     assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
 
 
+def test_text_body_height(browser):
+    demo_qa_page = DemoQa(browser)
+    elements_page = ElementPage(browser)
+    demo_qa_page.visit()
+    time.sleep(3)
+    # assert demo_qa_page.equal_url()
+    demo_qa_page.btn_elements.click()
+    time.sleep(3)
+  
+    assert elements_page.get_center_text.get_text() == 'Please select an item from left to start practice.'
+
+
+
 
 
