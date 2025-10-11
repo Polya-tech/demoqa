224d803a56347ca46d8560e3b7334dd6a21d11b6
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-11 11:44:04 +0300

    Update base_page.py
    8 урок

--- pages/base_page.py
+++ pages/base_page.py
@@ -23,4 +23,20 @@ class BasePage:
             return False
 
 
+    def back(self):
+        self.driver.back()
+
+
+    def forward(self):
+        self.driver.forward()
+
+
+    def refresh(self):
+        self.driver.refresh()
+
+
+    def get_title(self):
+        self.driver.title()
+
+
 
