725c103669392c1f01df84eead17200a6debd405
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-11 11:43:25 +0300

    Update elements_page.py
    7 урок

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,7 @@
-from pages.base_page import BasePage
 from components.components import WebElement
+from pages.base_page import BasePage
+
+
 
 class ElementPage(BasePage):
 
@@ -8,9 +10,8 @@ class ElementPage(BasePage):
         super().__init__(driver, self.base_url)
 
         self.get_center_text = WebElement(self.driver, ".col-12.mt-4.col-md-6")
-        # print()
-
-
-
-       
-
+        self.text_elements = WebElement(self.driver, '.col-12.mt-4.col-md-6')
+        self.icon = WebElement(self.driver, '#app > header > a > img')
+        self.btn_sidebar_first = WebElement(self.driver, 'div:nth-child(1) > span > div')
+        self.btn_sidebar_first_textbox = WebElement(self.driver, 'div:nth-child(1) > div > ul > #item-0 > span')
+        self.btn_sidebar_first_checkbox = WebElement(self.driver, 'div:nth-child(1) > div > ul > #item-1 > span')
