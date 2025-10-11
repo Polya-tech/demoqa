934e79b8e3fef89c9d529d8daea412033fa53b97
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-11 11:49:17 +0300

    Update test_visible_hw.py

--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -18,12 +18,11 @@ def test_visible_accordion_default(browser):
     accordion_page.visit()
     accordion_page.section_heading2.click()
 
-    time.sleep(2)
     assert accordion_page.section_content2.visible()
     assert accordion_page.section_content3.visible()
     time.sleep(2)
     accordion_page.section_heading2.click()
-    time.sleep(2)
+ 
     assert not accordion_page.section_content2.visible()
     assert not accordion_page.section_content3.visible()
     time.sleep(2)
