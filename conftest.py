0e7c114101a135249e92f04f6ceb6a96948a41d6
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-12 11:41:47 +0300

    Update conftest.py
    9 lsn

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
-import pytest
-from selenium import webdriver
+import pytest #импортируем pytest
+from selenium import webdriver #импортируем из selenium - webdriver
 
-@pytest.fixture(scope="session")
-def browser():
-    driver = webdriver.Chrome() #создаем драйвер
-    yield driver #yield - нужен чтобы драйвер мог использоваться в функции
-    driver.quit()
+@pytest.fixture(scope="session") #декоратор - это функция, которая изменяет поведение другой функциии. pytest.fixture - будет изменять поведение нашей функции в браузере. Передаем в scope - session
+def browser(): #пишем функцию чтобы использовать наш браузер в других тестах, и на нашу ф-и навешиваем декоратор (обертку)
+    driver = webdriver.Chrome()#пишем наш драйвер
+    driver.set_window_size(1000, 1000)
+    yield driver #чтобы наш драйвер мог использоваться в функции мы должны написать слово yield
+    driver.quit() #закрываем наш драйвер
