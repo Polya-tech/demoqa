010be0dab679db0bc8f1b7fe3bd6a670bb47563f
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-12 11:41:12 +0300

    Create modal_dialogs.py
    9 hw_lsn

---
+++ pages/modal_dialogs.py
@@ -0,0 +1,13 @@
+from components.components import WebElement
+from pages.base_page import BasePage
+
+class ModalDialogs(BasePage):
+
+
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/modal-dialogs'
+        super().__init__(driver, self.base_url)
+
+        self.btn_three_element = WebElement(self.driver, '#app > div > div > div > div:nth-child(1) > div > div > div:nth-child(3) > span')
+        self.btns_three_menu = WebElement(self.driver, 'div:nth-child(3) > div > ul > li')
+        self.icon = WebElement(self.driver, '#app > header > a > img')
