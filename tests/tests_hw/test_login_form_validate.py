1a5cf92533da8b0238a3c880e8cac24e0d67d850
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-14 20:40:24 +0300

    Update test_login_form_validate.py

--- tests/tests_hw/test_login_form_validate.py
+++ tests/tests_hw/test_login_form_validate.py
@@ -10,3 +10,16 @@ def test_practice_form(browser):
     assert test_pf.last_name.get_dom_attribute('placeholder') == 'Last Name'
     assert test_pf.email.get_dom_attribute('placeholder') == 'name@example.com'
 
+
+ first_name_placeholder = test_pf.first_name.get_dom_attribute('placeholder')
+    last_name_placeholder = test_pf.last_name.get_dom_attribute('placeholder')
+    email_placeholder = test_pf.email.get_dom_attribute('placeholder')
+
+    print("First name placeholder:", first_name_placeholder)
+    print("Last name placeholder:", last_name_placeholder)
+    print("Email placeholder:", email_placeholder)
+
+    assert first_name_placeholder == 'First Name'
+    assert last_name_placeholder == 'Last Name'
+    assert email_placeholder == 'name@example.com'
+
