9b1fb44736dd9e5ee46db82de142e218bd641bcb
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-14 20:31:59 +0300

    Update test_login_form_validate.py

--- tests/tests_hw/test_login_form_validate.py
+++ tests/tests_hw/test_login_form_validate.py
@@ -1,1 +1,12 @@
+from pages.practice_form import PracticeForm
+import time
+
+def test_practice_form(browser):
+    test_pf = PracticeForm(browser)
+
+    test_pf.visit()
+
+    assert test_pf.first_name.get_dom_attribute('placeholder') == 'First Name'
+    assert test_pf.last_name.get_dom_attribute('placeholder') == 'Last Name'
+    assert test_pf.email.get_dom_attribute('placeholder') == 'name@example.com'
 
