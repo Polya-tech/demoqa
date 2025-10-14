0b0fddd896a4d9f4f0fe16b85536204f989d258a
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-14 12:52:40 +0300

    Update test_text_box.py
    10 lsn

--- tests/tests_hw/test_text_box.py
+++ tests/tests_hw/test_text_box.py
@@ -4,14 +4,16 @@ import time
 def test_clear(browser):
     text_box = TextBox(browser)
 
-    full_name_text = "Иван"   # Переменные с тестовыми данными
-    address_text = "г. Москва, ул. Пушкина, д. 10"
+    # full_name_text = "Иван"   # Переменные с тестовыми данными
+    # address_text = "г. Москва, ул. Пушкина, д. 10"
 
     text_box.visit()
-    text_box.full_name.send_keys(full_name_text)
-    text_box.current_address.send_keys(address_text)
+    text_box.full_name.send_keys('Polina')
+    text_box.current_address.send_keys('д.Петушки, до 5')
     text_box.btn_submit.click()
     time.sleep(3)
 
-    assert text_box.output_full_name.get_text() == 'Name:Иван'
-    assert text_box.output_current_address.get_text() == 'Current Address :г. Москва, ул. Пушкина, д. 10'
+    assert text_box.output_full_name.get_text() == 'Name:Polina'
+    assert text_box.output_current_address.get_text() == 'Current Address :д.Петушки, до 5'
+
+
