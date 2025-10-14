316fa3d78a3929ae367fe27f6852d24432cda381
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-14 21:40:01 +0300

    Update test_login_form_validate.py

--- tests/tests_hw/test_login_form_validate.py
+++ tests/tests_hw/test_login_form_validate.py
@@ -6,20 +6,26 @@ def test_practice_form(browser):
 
     test_pf.visit()
 
+
     assert test_pf.first_name.get_dom_attribute('placeholder') == 'First Name'
     assert test_pf.last_name.get_dom_attribute('placeholder') == 'Last Name'
     assert test_pf.email.get_dom_attribute('placeholder') == 'name@example.com'
+    assert test_pf.email.get_dom_attribute('pattern') == r'^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$'
+
+    time.sleep(1)
+
+    test_pf.btn_submit_pf.click_force()
+    time.sleep(1)
+
+    assert test_pf.validated_form.get_dom_attribute('class') == 'was-validated'
 
 
- first_name_placeholder = test_pf.first_name.get_dom_attribute('placeholder')
-    last_name_placeholder = test_pf.last_name.get_dom_attribute('placeholder')
-    email_placeholder = test_pf.email.get_dom_attribute('placeholder')
 
-    print("First name placeholder:", first_name_placeholder)
-    print("Last name placeholder:", last_name_placeholder)
-    print("Email placeholder:", email_placeholder)
-
-    assert first_name_placeholder == 'First Name'
-    assert last_name_placeholder == 'Last Name'
-    assert email_placeholder == 'name@example.com'
-
+    # first_name_placeholder = test_pf.first_name.get_dom_attribute('placeholder')
+    # email_pattern = test_pf.email.get_dom_attribute('pattern')
+    
+    # print("First name placeholder:", first_name_placeholder)
+    # print("Email_pattern:", email_pattern)
+    
+    # assert first_name_placeholder == 'First Name'
+    # assert email_pattern == 'First Name'
