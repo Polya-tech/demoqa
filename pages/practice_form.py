021e3eade295ba08b0464bfae7fbe5105c1aa1c5
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-16 15:57:56 +0300

    Update practice_form.py
    10 домашка

--- pages/practice_form.py
+++ pages/practice_form.py
@@ -1,11 +1,20 @@
-from pages.practice_form import PracticeForm
-import time
+from components.components import WebElement
+from pages.base_page import BasePage
 
-def test_practice_form(browser):
-    test_pf = PracticeForm(browser)
+class PracticeForm(BasePage):
 
-    test_pf.visit()
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/automation-practice-form'
+        super().__init__(driver, self.base_url)
 
-    assert test_pf.first_name.get_dom_attribute('placeholder') == 'First Name'
-    assert test_pf.last_name.get_dom_attribute('placeholder') == 'Last Name'
-    assert test_pf.email.get_dom_attribute('placeholder') == 'name@example.com'
+        self.first_name = WebElement(driver, '#firstName')
+        self.last_name = WebElement(driver, '#lastName')
+        self.email = WebElement(driver, '#userEmail')
+        self.btn_submit_pf = WebElement(driver, '#submit')
+        self.validated_form = WebElement(driver, '#userForm')
+
+        self.element_state_and_city = WebElement(driver, '#stateCity-label')
+        self.state_container = WebElement(driver, '#state')
+        self.state_input = WebElement(driver, "input[id^='react-select-'][id$='-input']")
+        self.city_container = WebElement(driver, '#city') #div.css-1hwfws3
+        self.city_input = WebElement(driver, "input#react-select-4-input")
