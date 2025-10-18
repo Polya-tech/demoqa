014e9a0a327b7dafe6f64aac5f892249688bccc2
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-18 21:29:07 +0300

    Update tables.py
    11

--- pages/tables.py
+++ pages/tables.py
@@ -12,6 +12,15 @@ class Tables(BasePage):
         self.btn_delete_row = WebElement(driver, '//span[@title="Delete"]', 'xpath')
         self.btn_add = WebElement(driver, '#addNewRecordButton')
         self.modal_content = WebElement(driver, 'body > div.fade.modal.show > div > div')
-        self.reg_form_btn_submit = WebElement(driver, '#submit')
+        self.btn_submit = WebElement(driver, '#submit')
+        self.first_name = WebElement(driver, '#firstName')
+        self.last_name = WebElement(driver, '#lastName')
+        self.email = WebElement(driver, '#userEmail')
+        self.age = WebElement(driver, '#age')
+        self.salary = WebElement(driver, '#salary')
+        self.department = WebElement(driver, '#department')
+        self.new_note = WebElement(driver, '#app > div > div > div > div.col-12.mt-4.col-md-6 > div.web-tables-wrapper > div.ReactTable.-striped.-highlight > div.rt-table > div.rt-tbody > div:nth-child(3) > div')
+        self.pencil = WebElement(driver, '#edit-record-4')
+        self.btn_delete = WebElement(driver, '#delete-record-4')
 
 
