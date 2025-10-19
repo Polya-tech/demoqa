9b08475923387624207bab0fd321986e076bd0eb
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-19 20:54:55 +0300

    Update modal_dialogs.py
    12

--- pages/modal_dialogs.py
+++ pages/modal_dialogs.py
@@ -11,3 +11,7 @@ class ModalDialogs(BasePage):
         self.btn_three_element = WebElement(self.driver, '#app > div > div > div > div:nth-child(1) > div > div > div:nth-child(3) > span')
         self.btns_three_menu = WebElement(self.driver, 'div:nth-child(3) > div > ul > li')
         self.icon = WebElement(self.driver, '#app > header > a > img')
+        self.small_modal = WebElement(self.driver, '#showSmallModal')
+        self.large_modal = WebElement(self.driver, '#showLargeModal')
+        self.close_small_modal = WebElement(self.driver, '#closeSmallModal')
+        self.close_large_modal = WebElement(self.driver, '#closeLargeModal')
